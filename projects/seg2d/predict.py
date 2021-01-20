29c4f818bf448b8917de36d488c227420b056b3e
Yipeng Hu <yipeng.hu@ucl.ac.uk>
2021-01-20 19:58:33 +0000

    ref #0 predict added

---
+++ projects/seg2d/predict.py
@@ -0,0 +1,38 @@
+import os
+
+import tensorflow as tf
+from matplotlib import pyplot as plt
+
+from loader import H5DataLoader
+import utils
+
+save_path = "results"
+epoch = 79
+imported = tf.saved_model.load(os.path.join(save_path, 'epoch{:d}'.format(epoch)))
+
+
+data_files = {'test': 'dataset/test.h5', 'val': 'dataset/val.h5'}
+loader_val = H5DataLoader(data_files["val"], 3, training=False)
+loader_test = H5DataLoader(data_files["test"], 3, training=False)
+
+
+losses_all, metrics_all = [], []
+for idx, (frames_val, masks_val) in enumerate(loader_val):
+    
+    if idx%5 ==0:
+        preds_val = imported(frames_val, training=False)
+
+        losses = utils.dice_loss(preds_val, masks_val)
+        dices, false_positives = utils.dice_metric_fg(preds_val, masks_val)
+        print(losses)
+        print(dices, false_positives)
+
+        for dd in range(preds_val.shape[0]):
+            plt.figure()
+            plt.imshow(preds_val[dd,...])
+            plt.figure()
+            plt.imshow(masks_val[dd,...])
+            plt.figure()
+            plt.imshow(frames_val[dd,...])
+        plt.show()
+
