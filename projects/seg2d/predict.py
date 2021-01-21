886e26367160f67da8c13e64dcaebfdc1e131d75
Yipeng Hu <yipeng.hu@ucl.ac.uk>
2021-01-21 21:18:56 +0000

    ref #0 stats added in predict

--- projects/seg2d/predict.py
+++ projects/seg2d/predict.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import tensorflow as tf
 from matplotlib import pyplot as plt
@@ -6,33 +7,55 @@ from matplotlib import pyplot as plt
 from loader import H5DataLoader
 import utils
 
-save_path = "results"
-epoch = 79
+
+save_path = "results_not"
+epoch = 499
 seg_net_imported = tf.saved_model.load(os.path.join(save_path, 'epoch{:d}'.format(epoch)))
 
-
+batch_size = 1
 data_files = {'test': 'dataset/test.h5', 'val': 'dataset/val.h5'}
-loader_val = H5DataLoader(data_files["val"], 3, training=False)
-loader_test = H5DataLoader(data_files["test"], 3, training=False)
+# loader_test = H5DataLoader(data_files["val"], batch_size, training=False)
+loader_test = H5DataLoader(data_files["test"], batch_size, training=False)
 
 
-losses_all, metrics_all = [], []
-for idx, (frames_val, masks_val) in enumerate(loader_test):
+# stats
+losses_all, dices_all, false_positives_all = [], [], []
+for idx, (frames_test, masks_test) in enumerate(loader_test):
+
+    # t0 = time.time()
+    preds_test = seg_net_imported(frames_test, training=False)
+    # print('Inference time [%f]s' % (time.time()-t0))
+
+    losses = utils.dice_loss(preds_test, masks_test)
+    dices, false_positives = utils.dice_metric_fg(preds_test, masks_test)
+    losses_all += [losses]
+    dices_all += [dices]
+    false_positives_all += [false_positives]
+
+print('val-loss={:0.5f}, val-dice={:0.5f}, false_positives={:0.5f}'.format(
+    tf.reduce_mean(tf.concat(losses_all,axis=0)),
+    tf.reduce_mean(tf.concat(dices_all,axis=0)),
+    tf.reduce_mean(tf.concat(false_positives_all,axis=0))
+    ))
+
+
+# visualise
+for idx, (frames_test, masks_test) in enumerate(loader_test):
     
     if idx%5 ==0:
-        preds_val = seg_net_imported(frames_val, training=False)
+        preds_test = seg_net_imported(frames_test, training=False)
 
-        losses = utils.dice_loss(preds_val, masks_val)
-        dices, false_positives = utils.dice_metric_fg(preds_val, masks_val)
-        print(losses)
-        print(dices, false_positives)
+        losses = utils.dice_loss(preds_test, masks_test)
+        dices, false_positives = utils.dice_metric_fg(preds_test, masks_test)
+        print('losses: ',losses)
+        print('dices: ', dices)
+        print('false-spotives: ', false_positives)
 
-        for dd in range(preds_val.shape[0]):
+        for dd in range(preds_test.shape[0]):
             plt.figure()
-            plt.imshow(preds_val[dd,...])
+            plt.imshow(preds_test[dd,...])
             plt.figure()
-            plt.imshow(masks_val[dd,...])
+            plt.imshow(masks_test[dd,...])
             plt.figure()
-            plt.imshow(frames_val[dd,...])
+            plt.imshow(frames_test[dd,...])
         plt.show()
-
